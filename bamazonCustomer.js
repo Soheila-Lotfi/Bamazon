513724613ee62ec556a91dc303f67ec0824c93e1
soheila <soheilalotfi@ymail.com>
2019-08-13 15:33:22 -0400

    write start function

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -15,3 +15,22 @@ connection.connect(function(err) {
   if (err) throw err;
   Start();
 });
+
+function Start() {
+  connection.query(
+    "SELECT item_id, product_name, price FROM products",
+    function(err, res) {
+      if (err) throw err;
+      for (var i = 0; i < res.length; i++) {
+        console.log(
+          "Item_id: " +
+            res[i].item_id +
+            "|| Product_name: " +
+            res[i].product_name +
+            "|| Price: $" +
+            res[i].price
+        );
+      }
+    }
+  );
+}
