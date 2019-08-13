f762b762d8fb015d55b4248c547159d0df41aa1f
soheila <soheilalotfi@ymail.com>
2019-08-13 12:43:22 -0400

    create connection to database

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,2 +1,17 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+
+// ### Challenge #1: Customer View (Minimum Requirement)
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "Leila@1357",
+  database: "bamazon"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  Start();
+});
