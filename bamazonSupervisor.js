8689096023022d7a64a30bf2cf901e35abbbaf5d
soheila <soheilalotfi@ymail.com>
2019-08-18 12:42:18 -0400

    add  cosole.table to show the result in a table

--- bamazonSupervisor.js
+++ bamazonSupervisor.js
@@ -1,5 +1,6 @@
 var inquirer = require("inquirer");
 var mysql = require("mysql");
+var cTable = require("console.table");
 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -40,8 +41,24 @@ function Start() {
         console.log(querySelect);
         connection.query(querySelect, function(err, res) {
           if (err) throw err;
-          console.log(res);
+          console.table(res);
         });
       }
     });
 }
+
+// | department_id | department_name | over_head_costs | product_sales | total_profit |
+// | ------------- | --------------- | --------------- | ------------- | ------------ |
+// | 01            | Electronics     | 10000           | 20000         | 10000        |
+// | 02            | Clothing        | 60000           | 100000        | 40000        |
+
+// 5. The `total_profit` column should be calculated on the fly using the difference between `over_head_costs` and `product_sales`. `total_profit` should not be stored in any database. You should use a custom alias.
+
+// 6. If you can't get the table to display properly after a few hours, then feel free to go back and just add `total_profit` to the `departments` table.
+
+//
+
+//
+//
+
+//    * **HINT**: There may be an NPM package that can log the table to the console. What's is it? Good question :)
